b89932bfca2bbb081617e643fe8e1aeb8bc95624
Chirag Saini <chiragsaini7777@gmail.com>
2021-06-11 23:05:49 +0530

    Initial Setup with snowpack

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Hello World
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
