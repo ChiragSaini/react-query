7a0442ec1facb0fdfa873187367a6d8a97f88ce5
Chirag Saini <chiragsaini7777@gmail.com>
2021-06-12 13:15:45 +0530

    Lesson 8

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import './App.css';
 
-const Button = () => {
-    const { data, error } = useQuery('hello-world', () => {
-        return new Promise(resolve => setTimeout(() => resolve(Math.random()), 1000))
-    });
-    console.log({ data, error });
-    return <button>I am a Button {data}</button>
-}
-
-
 function App() {
-
-    const [visible, setVisible] = useState(true);
-    function toggleButton(){
-        setVisible(state => !state);
-    }
-
+    useQuery(['todo-single-item', 'itemId']) // Dont use objects. Stick to primitives
     return (
         <div className="App">
             <header className="App-header">
-                {visible && <Button />}
-                <button onClick={toggleButton}>Toggle Button</button>
+                <button>Toggle Button</button>
                 <p>
                     Hello World
                 </p>
