c96e29adcbee65746be4e4f77101ca0b1078635c
Chirag Saini <chiragsaini7777@gmail.com>
2021-06-12 13:49:53 +0530

    Updated Master to barebones

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useQuery } from 'react-query';
 import './App.css';
-
-const Button = () => {
-    const { data, error } = useQuery('hello-world', () => {
-        return new Promise(resolve => setTimeout(() => resolve(Math.random()), 1000))
-    });
-    console.log({ data, error });
-    return <button>I am a Button {data}</button>
-}
-
-
 function App() {
-
-    const [visible, setVisible] = useState(true);
-    function toggleButton(){
-        setVisible(state => !state);
-    }
 
     return (
         <div className="App">
             <header className="App-header">
-                {visible && <Button />}
-                <button onClick={toggleButton}>Toggle Button</button>
                 <p>
                     Hello World
                 </p>
