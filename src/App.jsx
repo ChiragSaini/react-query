83acbe196790c736e3ca225d86f4a88da0dfa33f
Chirag Saini <chiragsaini7777@gmail.com>
2021-06-12 13:52:05 +0530

    Even barebones master

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,7 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <p>
-                    Hello World
-                </p>
-            </header>
+            <h2>Hello World</h2>
         </div>
     );
 }
